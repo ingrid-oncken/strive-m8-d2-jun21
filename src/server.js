43b69e30b9c21a0fe820a30b16f0d80197acca0b
Riccardo Gulin <riccardo@nucleode.com>
2021-11-02 11:00:30 +0100

    Adds server's stuff and basic packages

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import listEndpoints from "express-list-endpoints"
+
+import { unauthorizedHandler, forbiddenHandler, catchAllHandler } from "./errorHandlers.js"
+
+const server = express()
+const port = process.env.PORT || 3001
+
+// ******************** MIDDLEWARES *************************+
+
+server.use(cors())
+server.use(express.json())
+
+// ******************** ROUTES ******************************
+
+// ********************** ERROR HANDLERS *************************
+
+server.use(unauthorizedHandler)
+server.use(forbiddenHandler)
+server.use(catchAllHandler)
+
+console.table(listEndpoints(server))
+
+mongoose.connect(process.env.MONGO_CONNECTION)
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongo connected!")
+  server.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+  })
+})
